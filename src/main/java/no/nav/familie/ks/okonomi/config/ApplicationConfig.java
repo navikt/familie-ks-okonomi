36c77b14a10f9a309861e3dcb2efe5ad61e3f875
Helga Nyrud <helganyrud@Helgas-MacBook-Pro.local>
2019-08-15 13:24:41 +0200

    Lagt til postgres-database med roterende passord mot vault

--- src/main/java/no/nav/familie/ks/okonomi/config/ApplicationConfig.java
+++ src/main/java/no/nav/familie/ks/okonomi/config/ApplicationConfig.java
@@ -5,8 +5,10 @@ import org.springframework.boot.web.embedded.jetty.JettyServletWebServerFactory;
 import org.springframework.boot.web.servlet.server.ServletWebServerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootConfiguration
+@EnableJpaAuditing
 @ComponentScan({"no.nav.familie.ks.okonomi"})
 public class ApplicationConfig {
 
---
+++ src/test/java/no/nav/familie/ks/okonomi/DevLauncher.java
@@ -0,0 +1,20 @@
+package no.nav.familie.ks.okonomi;
+
+import no.nav.familie.ks.okonomi.config.ApplicationConfig;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.error.ErrorMvcAutoConfiguration;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.test.autoconfigure.orm.jpa.AutoConfigureDataJpa;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+@SpringBootApplication(exclude = ErrorMvcAutoConfiguration.class)
+@EnableTransactionManagement
+@AutoConfigureDataJpa
+public class DevLauncher {
+
+    public static void main(String... args) {
+        new SpringApplicationBuilder(ApplicationConfig.class)
+                .profiles("dev")
+                .run(args);
+    }
+}
