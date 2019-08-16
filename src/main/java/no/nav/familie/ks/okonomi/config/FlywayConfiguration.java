4ccea535b740b457baacc70888274257425bd213
Martine Enger <marenger@student.matnat.uio.no>
2019-08-16 13:50:36 +0200

    Kjører flyway lokalt for å slippe å duplisere alle sql-scriptene

--- src/main/java/no/nav/familie/ks/okonomi/config/FlywayConfiguration.java
+++ src/main/java/no/nav/familie/ks/okonomi/config/FlywayConfiguration.java
@@ -5,8 +5,10 @@ import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.flyway.FlywayConfigurationCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("!dev")
 @ConditionalOnProperty("spring.flyway.enabled")
 public class FlywayConfiguration {
 
