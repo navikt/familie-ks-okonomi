cc9cdd2e155366d1bb43059255f900d60c6f57f7
Martine Enger <marenger@student.matnat.uio.no>
2019-08-19 10:18:16 +0200

    Legg på logg for å sjekke om flyway-konfigurasjon kjøres opp i preprod. Må legge på EnableJdbcRepositories ref navs postgres-dok

--- src/main/java/no/nav/familie/ks/okonomi/config/FlywayConfiguration.java
+++ src/main/java/no/nav/familie/ks/okonomi/config/FlywayConfiguration.java
@@ -1,19 +1,26 @@
 package no.nav.familie.ks.okonomi.config;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.autoconfigure.flyway.FlywayConfigurationCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
 
 @Configuration
 @Profile("!dev")
 @ConditionalOnProperty("spring.flyway.enabled")
+@EnableJdbcRepositories
 public class FlywayConfiguration {
+
+    private static final Logger LOG = LoggerFactory.getLogger(FlywayConfiguration.class);
 
     @Bean
     public FlywayConfigurationCustomizer flywayConfig(@Value("${spring.cloud.vault.database.role}") String role) {
+        LOG.info("Setter rolle på database før flywaymigrering");
         return c -> c.initSql(String.format("SET ROLE \"%s\"", role));
     }
 }
